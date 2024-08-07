716c240c9a27afcd24c3c43d67b4b8e46e93fad9
Tom Kwok <work@tomkwok.xyz>
2024-08-07 19:54:18 +1000

    Add sample response from LLM

--- src/pages/Content/index.js
+++ src/pages/Content/index.js
@@ -1,6 +1,8 @@
-import { printLine } from './modules/print';
+// import { printLine } from './modules/print';
+//
+// console.log('Content script works!');
+// console.log('Must reload extension for modifications to take effect.');
+//
+// printLine("Using the 'printLine' function from the Print Module");
 
 console.log('Content script works!');
-console.log('Must reload extension for modifications to take effect.');
-
-printLine("Using the 'printLine' function from the Print Module");
